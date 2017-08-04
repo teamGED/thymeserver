f64829330be261541333ead75aeb5f8a442adf6d
Connor <connor.alcock.87@gmail.com>
2017-08-04 14:58:13 -0600

    SERVER IS WORKING

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../db/knex');
 
 //routes
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json('index');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const knex = require('../db/knex');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
