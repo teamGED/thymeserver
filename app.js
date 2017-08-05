16bbbd23ad0a67ebe26594a615d625458a7ce785
EllenDown <ellenldowning@gmail.com>
2017-08-05 13:25:27 -0600

    commit changes made to files

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+const app = express
+
+const users = require('./api/users')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,12 +18,14 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+
+//maybe delete this
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//mount the router
 
+app.use('/api/v1/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -34,13 +35,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
 });
 
 module.exports = app;
--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
-
-module.exports = require('knex')(config);
+const environmentConfig = config[environment];
+const knex = require('knex');
+const connection = knex(environmentConfig);
+module.exports = require('knex')(environmentConfig);
